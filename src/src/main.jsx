1f41f4410f9ff22c30967ce00459be974e4f0487
regelus19 <47474147+regelus19@users.noreply.github.com>
2026-02-21 16:41:37 -0600

    Create main.jsx

---
+++ src/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../TexasPrimarySimulator_v3.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+```
+
+Commit message:
+```
+Add src/main.jsx entry point
